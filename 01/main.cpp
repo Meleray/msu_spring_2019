bf3b58f9dc05bb326be9ca8c99bb6e026dfca400
Konov Mike <48065963+Meleray@users.noreply.github.com>
2019-03-21 18:11:56 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -2,31 +2,32 @@
 #include <iostream>
 #include <vector>
 
+const int MAX_N = 100001;
+
+void Eratosphen(std::vector<bool> & p)
+{
+    p[0] = p[1] = false;
+    for(int i = 2; i < MAX_N; ++i){
+        if(p[i])
+            if((long long)i * i < MAX_N)
+                for (int j = i * i; j < MAX_N; j += i)
+                    p[j] = false;
+    }
+}
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    vector<int> p(100001, true);
-    p[0] = p[1] = false;
-    for(int i = 2; i < 100001; ++i){
-        if(p[i])
-            if((long long)i * i <= 100000)
-                for (int j = i * i; j < 100001; j += i)
-                    p[j] = false;
-    }
-    vector<int> in;
-    for (int i = 1; i < argc; ++i)
-    {
-        int v = std::atoi(argv[i]);
-        in.push_back(v);
-    }
-    int n = in.size();
+    vector<bool> p(MAX_N, true);
+    Eratosphen(p);
+    int n = argc - 1;
     if (n % 2 == 1 || n == 0)
         return -1;
     for (int i = 0; i < n; i += 2)
     {
         int ans = 0;
-        int a = in[i], b = in[i + 1], j = 0;
+        int a = std::atoi(argv[i + 1]), b = std::atoi(argv[i + 2]), j = 0;
         while (j < Size && Data[j] != a)
             ++j;
         while (j < Size && Data[j] != b)
