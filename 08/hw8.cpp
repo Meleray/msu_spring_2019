eaf47ee2b0727351fdf62e429d3afafdfbb8efbe
Konov Mike <48065963+Meleray@users.noreply.github.com>
2019-04-25 18:12:14 +0300

    Update hw8.cpp

--- 08/hw8.cpp
+++ 08/hw8.cpp
@@ -3,30 +3,40 @@
 #include <mutex>
 #include <thread>
 
-int main(void)
-{
-	std::mutex m;
-	std::condition_variable cv;
-	bool notified = false;
-	std::thread t([&] {
-		for (int i = 0; i < 500000; ++i) {
-			std::unique_lock<std::mutex> l(m);
-			cv.wait(l, [&notified] { return !notified; });
-			cout << "Ping" << endl;
-			notified = true;
-			l.unlock();
-			cv.notify_all();
-		}
-	});
+using namespace std;
 
-	for (int i = 0; i < 500000; ++i) {
-		std::unique_lock<std::mutex> l(m);
-		cv.wait(l, [&notified] { return notified; });
-		cout << "Pong" << endl;
-		notified = false;
-		l.unlock();
-		cv.notify_all();
-	}
-	t.join();
-	return 0;
+const int num = 500000;
+std::mutex m;
+std::condition_variable cv;
+bool notified = false;
+
+void ping()
+{
+    for (int i = 0; i < num; ++i)
+    {
+        std::unique_lock<std::mutex> l(m);
+        while (notified)
+            cv.wait(l);
+        cout << "ping" << endl;
+        notified = true;
+        l.unlock();
+        cv.notify_all();
+    }
+}
+
+int main()
+{
+    std::thread t(ping);
+    for (int i = 0; i < num; ++i)
+    {
+	std::unique_lock<std::mutex> l(m);
+	while (!notified)
+        cv.wait(l);
+	cout << "pong" << endl;
+	notified = false;
+	l.unlock();
+	cv.notify_all();
+    }
+    t.join();
+    return 0;
 }
