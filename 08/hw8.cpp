e684aeec36e272ff6723e658d9b98de432e3691d
Konov Mike <48065963+Meleray@users.noreply.github.com>
2019-05-07 19:15:09 +0300

    Update hw8.cpp

--- 08/hw8.cpp
+++ 08/hw8.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <string>
 
 using namespace std;
 
@@ -10,31 +11,31 @@ std::mutex m;
 std::condition_variable cv;
 bool notified = false;
 
-void ping()
+void write(string a)
 {
     for (int i = 0; i < num; ++i)
     {
         std::unique_lock<std::mutex> l(m);
-        while (notified)
-            cv.wait(l);
-        cout << "ping" << endl;
-        notified = true;
+        if (a == "ping")
+        {
+            while (notified)
+                cv.wait(l);
+        }
+        else
+        {
+            while (!notified)
+                cv.wait(l);
+        }
+        cout << a << endl;
+        notified = (a == "ping");
         cv.notify_all();
     }
 }
 
 int main()
 {
-    std::thread t(ping);
-    for (int i = 0; i < num; ++i)
-    {
-	std::unique_lock<std::mutex> l(m);
-	while (!notified)
-        cv.wait(l);
-	cout << "pong" << endl;
-	notified = false;
-	cv.notify_all();
-    }
+    std::thread t(write, "ping");
+    write("pong");
     t.join();
     return 0;
 }
