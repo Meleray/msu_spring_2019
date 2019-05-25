51a90f09e313f7836b699929914954991c1f1300
Konov Mike <48065963+Meleray@users.noreply.github.com>
2019-05-25 08:58:33 +0300

    Update main.cpp

--- 09/main.cpp
+++ 09/main.cpp
@@ -18,21 +18,21 @@ const uint64_t MaxN = numeric_limits<uint64_t>::max();
 void Count(string name)
 {
 	for (uint64_t curr = 0; curr <= MaxN; ++curr)
-    {
+    	{
 		unique_lock<mutex> lock(m);
 		while (!outend)
-            processed.wait(lock);
+            		processed.wait(lock);
 		counter = 0;
 		ifstream in(name, std::ios::binary | std::ios::in);
 		uint64_t x;
 		for (; in.read((char *) &x, sizeof(uint64_t));)
-        {
+        	{
 			if (x == curr)
-                ++counter;
+                		++counter;
 		}
 		in.close();
 		lock.unlock();
-        ready = true;
+        	ready = true;
 		outend = false;
 		processed.notify_one();
 	}
@@ -42,13 +42,13 @@ void Out(string name)
 {
 	ofstream out(name);
 	for (uint64_t curr = 0; curr <= MaxN; ++curr)
-    {
+    	{
 		unique_lock<mutex> lock(m);
 		outend = true;
 		processed.notify_one();
 		while (!ready)
-            processed.wait(lock);
-        uint64_t tmp = counter;
+            		processed.wait(lock);
+        	uint64_t tmp = counter;
 		for (uint64_t i = 0; i < tmp; ++i)
 			out << curr << ' ';
 		ready = false;
